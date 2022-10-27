f82c60211fffc283ce4452720bd94d4f952b7e04
deeparka <arkadeep180@gmail.com>
2022-10-27 13:41:22 +0530

    completed the sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,32 @@
+import Twitter from "../images/twitter.png";
+import SidebarRow from "./SidebarRow";
+import {
+    BookmarkIcon,
+    HomeIcon,
+    HashtagIcon,
+    BellIcon,
+    MailIcon,
+    CollectionIcon,
+    UserIcon,
+    DotsCircleHorizontalIcon,
+} from "@heroicons/react/outline";
+
+function Sidebar() {
+    return (
+        <div className="flex flex-col">
+            <img className="h-10 w-10" src={Twitter.src} alt="Twitter.png" />
+            <SidebarRow Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={HashtagIcon} title="Explore" />
+            <SidebarRow Icon={BellIcon} title="Notifications" />
+            <SidebarRow Icon={MailIcon} title="Messages" />
+            <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+            <SidebarRow Icon={CollectionIcon} title="Lists" />
+
+            <SidebarRow Icon={UserIcon} title="Sign In" />
+
+            <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ type-heroicons.d.ts
@@ -0,0 +1,15 @@
+declare module "@heroicons/react/*" {
+    const content: any;
+    export default content;
+}
+
+declare module "@heroicons/react/outline" {
+    export { default as HomeIcon } from "@heroicons/react/outline/HomeIcon.js";
+    export { default as HashtagIcon } from "@heroicons/react/outline/HashtagIcon.js";
+    export { default as BellIcon } from "@heroicons/react/outline/BellIcon.js";
+    export { default as MailIcon } from "@heroicons/react/outline/MailIcon.js";
+    export { default as BookmarkIcon } from "@heroicons/react/outline/BookmarkIcon.js";
+    export { default as CollectionIcon } from "@heroicons/react/outline/CollectionIcon.js";
+    export { default as UserIcon } from "@heroicons/react/outline/UserIcon.js";
+    export { default as DotsCircleHorizontalIcon } from "@heroicons/react/outline/DotsCircleHorizontalIcon.js";
+}
