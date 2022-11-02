1d7a39eed06768d67d2f3c9f320b2d3537f165ce
deeparka <arkadeep180@gmail.com>
2022-11-02 11:38:56 +0530

    completed image url section in TweetBox

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -5,13 +5,33 @@ import {
     CalendarIcon,
     LocationMarkerIcon,
 } from "@heroicons/react/outline";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Face_Placeholder from "../images/face_placeholder.jpeg";
 import { useSession } from "next-auth/react";
 
 function TweetBox() {
     const [input, setInput] = useState<string>(``);
+    const [image, setImage] = useState<string>(``);
+
     const { data: session } = useSession();
+
+    const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false);
+
+    const imageInputRef = useRef<HTMLInputElement>(null);
+
+    const addImageToTweet = (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+        e.preventDefault();
+
+        if (!imageInputRef.current?.value) return;
+
+        setImage(imageInputRef.current.value);
+
+        imageInputRef.current.value = "";
+
+        setImageUrlBoxIsOpen(false);
+    };
 
     return (
         <div className="flex space-x-2 p-5">
@@ -33,6 +53,9 @@ function TweetBox() {
                     <div className="flex items-center">
                         <div className="flex space-x-2 text-twitter flex-1">
                             <PhotographIcon
+                                onClick={() =>
+                                    setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)
+                                }
                                 className="h-5 w-5 cursor-pointer 
                             transition-transform duration-150 ease-out 
                             hover:scale-150"
@@ -50,6 +73,32 @@ function TweetBox() {
                             Tweet
                         </button>
                     </div>
+                    {imageUrlBoxIsOpen && (
+                        <form className="mt-5 flex rounded-lg bg-twitter/80 py-2 px-4">
+                            <input
+                                ref={imageInputRef}
+                                type="text"
+                                placeholder="Enter Image URL..."
+                                className="flex-1 bg-transparent p-2 
+                                text-white outline-none placeholder:text-white"
+                            />
+                            <button
+                                type="submit"
+                                onClick={addImageToTweet}
+                                className="font-bold text-white"
+                            >
+                                Add Image
+                            </button>
+                        </form>
+                    )}
+                    {image && (
+                        <img
+                            src={image}
+                            className="mt-10 h-40 w-full rounded-xl
+                            object-contain shadow-lg"
+                            alt=""
+                        />
+                    )}
                 </form>
             </div>
         </div>
